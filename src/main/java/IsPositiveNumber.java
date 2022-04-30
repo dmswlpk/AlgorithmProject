ed6389cb94fe2fe0573f42f1cfc777a224e503ca
dmswlpk <103320248+dmswlpk@users.noreply.github.com>
2022-04-30 11:46:41 -0600

    file number changed

--- src/main/java/IsPositiveNumber.java
+++ src/main/java/IsPositiveNumber.java
@@ -1,6 +1,7 @@
 public class IsPositiveNumber {
 
-    public boolean isPositiveNumberAlgorithm(int a){ /** Напишите алгоритм IsPositiveNumber, который возвращает true,
+    public boolean isPositiveNumberAlgorithm(int a){
+        /** Напишите алгоритм IsPositiveNumber, который возвращает true,
      если численный  параметр больше или равен нулю, и возвращает false, если параметр меньше 0.
      Проверьте работу метода на числах 555, 0 и -555. */
 
